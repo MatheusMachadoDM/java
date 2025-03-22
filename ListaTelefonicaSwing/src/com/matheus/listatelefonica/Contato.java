97f5f89c721809a10f65d2968d4b6850fbd07993
MatheusMachadoDM <165315314+MatheusMachadoDM@users.noreply.github.com>
2025-03-22 14:37:14 -0300

    Adicionando funcionalidade add

--- ListaTelefonicaSwing/src/com/matheus/listatelefonica/Contato.java
+++ ListaTelefonicaSwing/src/com/matheus/listatelefonica/Contato.java
@@ -1,32 +1,16 @@
 package com.matheus.listatelefonica;
 
 public class Contato {
-	private String nome, sobrenome, numeroTelefone;
+	private String nome, numeroTelefone;
 
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
-		if(nome.length() < 33) {
-			this.nome = nome;
-		}else {
-			throw new IllegalArgumentException("Nome ultrapassa o máximo de caracteres");
-		}
+		this.nome = nome;
+	
 	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		if(sobrenome.length() < 33) {
-			this.sobrenome = sobrenome;
-		}else {
-			throw new IllegalArgumentException("Sobrenome ultrapassa o máximo de caracteres");
-		}
-	}
-
 	public String getNumeroTelefone() {
 		return numeroTelefone;
 	}
@@ -34,7 +18,9 @@ public class Contato {
 	public void setNumeroTelefone(String numeroTelefone) {
 		this.numeroTelefone = numeroTelefone;
 	}
-	
+	public String toString() {
+		return nome + "--" + numeroTelefone;
+	}
 	
 	
 	
--- ListaTelefonicaSwing/src/com/matheus/listatelefonica/Main.java
+++ ListaTelefonicaSwing/src/com/matheus/listatelefonica/Main.java
@@ -1,7 +1,6 @@
 package com.matheus.listatelefonica;
 
 import java.util.Scanner;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,110 +12,151 @@ import javax.swing.JTable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-class BotaoAdicionar implements ActionListener{
+public class Main {
+	public static void main(String[] args) {
+			
+			JFrame janela = new JFrame();
+			JPanel painelAdd = new JPanel();
+			JPanel painelExcluir = new JPanel();
+			
+			painelAdd.setLayout(null);
+	        painelAdd.setBounds(10, 40, 380, 80);
+	        painelAdd.setVisible(false);
+	        
+	        painelExcluir.setLayout(null);
+	        painelExcluir.setBounds(10, 40, 380, 80);
+	        painelExcluir.setVisible(false);
 
-	private JFrame janela;
-	private JPanel painel;
-	
-	public BotaoAdicionar(JFrame janela, JPanel painel) {
-		this.janela = janela;
-		this.painel = painel;
+			List<String> listaNomes = new ArrayList<>();
+			List<String> listaNumeros = new ArrayList<>();
+			
+			JList<String> listaContato = new JList<>(listaNomes.toArray(new String[0]));
+			
+			FuncionalidadeAdicionar funcionalidadeAdicionar = new FuncionalidadeAdicionar(painelAdd, painelExcluir, listaNomes, listaNumeros, listaContato);
+			 
+	        JButton botaoAdicionar = new JButton("Adicionar");
+	        botaoAdicionar.addActionListener(funcionalidadeAdicionar);
+	        
+			JButton botaoExcluir = new JButton("Excluir");
+			botaoExcluir.addActionListener(new BotaoExcluir(painelExcluir, painelAdd));
+			
+			botaoAdicionar.setBounds(10, 10, 100,30);
+			botaoExcluir.setBounds(110, 10, 100,30);
+			
+			JScrollPane listaScroll = new JScrollPane(listaContato);
+			
+			listaScroll.setBounds(10, 120, 360, 400);
+			
+			janela.getContentPane().add(painelAdd);
+			janela.getContentPane().add(painelExcluir);
+			janela.getContentPane().add(listaScroll);
+			janela.add(botaoExcluir);
+			janela.add(botaoAdicionar);
+			janela.setLayout(null);
+			janela.setBounds(0, 0, 400, 600);
+			janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			janela.setVisible(true);
+			
 	}
+	
+}
+class FuncionalidadeAdicionar implements ActionListener{
+
+	private JPanel painelAtivar, painelDesativar;
+	private JTextField textoNome, textoNumero;
+	private List<String> listaNomes, listaNumeros;
+	private JList<String> listaContato;
+	
+	public FuncionalidadeAdicionar(JPanel painelAtivar, JPanel painelDesativar, List<String> listaNomes, List<String> listaNumeros, JList<String> listaContato) {
+		this.painelAtivar = painelAtivar;
+		this.painelDesativar = painelDesativar;
+		this.listaNomes = listaNomes;
+        this.listaNumeros = listaNumeros;
+        this.listaContato = listaContato;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		painel.setVisible(true);
+		painelAtivar.setVisible(true);
+		painelAtivar.setEnabled(true);
+		painelDesativar.setVisible(false);
+		painelDesativar.setEnabled(false);
 		
 		JButton botaoConfirmar =  new JButton("Add");
-		botaoConfirmar.setBounds(310, 70, 60, 29);
+		botaoConfirmar.setBounds(300, 40, 59, 29);
 				
 		JTextField textoNome = new JTextField();
 		JTextField textoNumero = new JTextField();
-		textoNome.setBounds(10, 70, 150, 30);
-		textoNumero.setBounds(160, 70, 150, 30);
+		textoNome.setBounds(0, 40, 150, 30);
+		textoNumero.setBounds(150, 40, 150, 30);
+		this.textoNome = textoNome;
+		this.textoNumero = textoNumero;
 		
 		JLabel nomeLabel = new JLabel("Nome: ");
 		JLabel numeroLabel = new JLabel("Número: ");
-		nomeLabel.setBounds(10, 40, 150, 30);
-		numeroLabel.setBounds(160, 40, 150, 30);
+		nomeLabel.setBounds(0, 10, 150, 30);
+		numeroLabel.setBounds(160, 10, 150, 30);
 		
-		painel.add(botaoConfirmar);
-		painel.add(numeroLabel);
-		painel.add(textoNumero);
-		painel.add(nomeLabel);
-		painel.add(textoNome);
-		painel.revalidate();
-        painel.repaint();
+		botaoConfirmar.addActionListener(e1 -> {
+			String nome = textoNome.getText();
+			String numero = textoNumero.getText();
+			
+			textoNome.setText("");
+			textoNumero.setText("");
+			
+			listaNomes.add(nome);
+			listaNumeros.add(numero);
+			listaContato.setListData(listaNomes.toArray(new String[0]));
+			painelAtivar.setVisible(false);
+            painelAtivar.setEnabled(false);
+            
+            
+		});
 		
-	}
+		painelAtivar.add(botaoConfirmar);
+		painelAtivar.add(numeroLabel);
+		painelAtivar.add(textoNumero);
+		painelAtivar.add(nomeLabel);
+		painelAtivar.add(textoNome);
+		painelAtivar.revalidate();
+		painelAtivar.repaint();
 	
+	}
+
+
 }
 
 class BotaoExcluir implements ActionListener{
 	
-	private JFrame janela;
+	private JPanel painelAtivar, painelDesativar;
 	
-	public BotaoExcluir(JFrame janela) {
-		this.janela = janela;
+	public BotaoExcluir(JPanel painelAtivar, JPanel painelDesativar) {
+		this.painelAtivar = painelAtivar;
+		this.painelDesativar = painelDesativar;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		painelAtivar.setVisible(true);
+		painelAtivar.setEnabled(true);
+		painelDesativar.setVisible(false);
+		painelDesativar.setEnabled(false);
+		
 		JButton botaoConfirmar =  new JButton("Deletar");
-		botaoConfirmar.setBounds(260, 70, 110, 29);
+		botaoConfirmar.setBounds(250, 40, 109, 29);
 		
 		JTextField textoNome = new JTextField();
-		textoNome.setBounds(10, 70, 250, 30);
+		textoNome.setBounds(0, 40, 250, 30);
 		
 		JLabel nomeLabel = new JLabel("Confirme o nome para excluir: ");
-		nomeLabel.setBounds(10, 40, 250, 30);
+		nomeLabel.setBounds(0, 10, 250, 30);
 		
-		janela.add(botaoConfirmar);
-		janela.add(nomeLabel);
-		janela.add(textoNome);
-		janela.revalidate();
-        janela.repaint();
+		painelAtivar.add(botaoConfirmar);
+		painelAtivar.add(nomeLabel);
+		painelAtivar.add(textoNome);
+		painelAtivar.revalidate();
+		painelAtivar.repaint();
 	}
-}
-
-public class Main {
-public static void main(String[] args) {
-		
-		JFrame janela = new JFrame();
-		JPanel painel = new JPanel();
-		
-		painel.setLayout(null);
-        painel.setBounds(10, 50, 380, 150);
-        painel.setVisible(false);
-		
-		JButton botaoAdicionar = new JButton("Adicionar");
-		botaoAdicionar.addActionListener(new BotaoAdicionar(janela, painel));
-		JButton botaoExcluir = new JButton("Excluir");
-		botaoExcluir.addActionListener(new BotaoExcluir(janela));
-		
-		botaoAdicionar.setBounds(10, 10, 100,30);
-		botaoExcluir.setBounds(110, 10, 100,30);
-		
-		String[] lista = {"ana", "Jose"};
-		
-		JList<String> listaContato = new JList<>(lista);
-		JScrollPane listaScroll = new JScrollPane(listaContato);
-		
-		listaScroll.setBounds(10, 120, 360, 400);
-		
-		janela.getContentPane().add(painel);
-		janela.getContentPane().add(listaScroll);
-		janela.add(botaoExcluir);
-		janela.add(botaoAdicionar);
-		janela.setLayout(null);
-		janela.setBounds(0, 0, 400, 600);
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		janela.setVisible(true);
-		
-		
-		
-		
-		
-		
-	}
-
 }
